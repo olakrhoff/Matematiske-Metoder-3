371ffcdb76b9e6bc7f32777723ed789b64d94a4d
Ola Kr. Hoff <50301728+olakrhoff@users.noreply.github.com>
2021-10-20 09:32:58 +0200

    Add files via upload

---
+++ Oblig 5/12.1CP1ab.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def PowerIterationMethod(A):
+    MAX_ITR = 10000
+    (m, n) = A.shape
+    x0 = np.matrix(np.ones(A.shape[0])).reshape(n, 1)
+    lamda = 0
+    for j in range(1, MAX_ITR):
+        u = x0 / np.linalg.norm(x0, 2)
+        x0 = A @ u
+        lamda = u.T @ x0
+        #lamda = u.T @ A @ u
+
+    u = x0 / np.linalg.norm(x0, 2)
+    return u, lamda
+
+
+A = np.matrix("10, -12, -6; 5, -5, -4; -1, 0, 3")
+B = np.matrix("-14, 20, 10; -19, 27, 12; 23, -32, -13")
+
+u, lamda = PowerIterationMethod(A)
+print(u)
+print(lamda)
+
+print()
+
+u, lamda = PowerIterationMethod(B)
+print(u)
+print(lamda)
---
+++ Oblig 5/4.3CP2.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+def ClassicGramSchmidt(A):
+    m, n = A.shape
+    r = np.matrix(np.zeros(n * n)).reshape((n, n))
+    q = np.matrix(np.zeros(m * n)).reshape(m, n)
+
+    for j in range(n):
+        y = A[:, j].reshape(1, n)
+        for i in range(j):
+            r[i, j] = q[:][i] @ y.T
+            y = y - r[i, j] * q[:][i]
+        r[j, j] = np.linalg.norm(y, 2)
+        q[:][j] = y * (1 / r[j, j])
+
+    return q.T, r
+
+A = np.matrix("4, 0; 3, 1")
+
+
+q, r = ClassicGramSchmidt(A)
+print("A:\n", A)
+print("Q:\n", q)
+print("R:\n", r)
